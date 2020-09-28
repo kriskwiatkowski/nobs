24408329a59bc2679bce4235ca4d58ac33c1dbfb
Kris Kwiatkowski <contact@amongbytes.com>
2020-09-28 21:03:08 +0100

    Use bits.RotateLeft64 whenever possible

--- hash/sha3/keccakf_amd64.go
+++ hash/sha3/keccakf_amd64.go
@@ -2,12 +2,11 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build amd64,!appengine,!gccgo
+// +build amd64,!appengine,!gccgo,!noasm
 
 package sha3
 
 // This function is implemented in keccakf_amd64.s.
 
 //go:noescape
-
 func keccakF1600(a *[25]uint64)
